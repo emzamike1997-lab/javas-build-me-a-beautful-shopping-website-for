9af9dc77e7b2fc3684d35e204fbe89f9e8360312
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-17 12:48:23 +0100

    JAVAS: Add app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+```python
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from config import config
+
+db = SQLAlchemy()
+ma = Marshmallow()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    db.init_app(app)
+    ma.init_app(app)
+
+    from app.routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+```
+
+###
