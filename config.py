419e377f576ef9b8e925a71d0140c57e41f1ec6d
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-17 12:48:22 +0100

    JAVAS: Add config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+```python
+import os
+
+class Config:
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': Config
+}
+```
+
+###
