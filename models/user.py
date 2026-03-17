775c583eb9677d3b074b75c4a90d05e1ec99277c
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-17 12:48:25 +0100

    JAVAS: Add models/user.py

---
+++ models/user.py
@@ -0,0 +1,26 @@
+```python
+from app import db
+from marshmallow import fields, Schema
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    email = fields.Str(required=True)
+```
+
+###
