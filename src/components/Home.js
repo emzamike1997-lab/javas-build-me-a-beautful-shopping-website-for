858a2cad22c74e1bce1eebf42a0cf8866124c589
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-17 12:48:10 +0100

    JAVAS: Add src/components/Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+```javascript
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to our shopping website!</h1>
+      <p>Browse our products and enjoy shopping with us.</p>
+    </div>
+  );
+}
+
+export default Home;
+```
+
+###
